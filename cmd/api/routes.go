f6c741d6732c7fb86c7e07da4db1068dce3401d5
ArtemGuschin8876 <newmsiartem@gmail.com>
2024-06-04 19:28:14 +0400

    add swagger

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,6 +6,8 @@ func (app *application) routes() *echo.Echo {
 	e := echo.New()
 
 	e.GET("/users", app.showUsersHandler)
+	e.Static("/swagger/", "C:/Users/rxri2/go/projects/GameMasterWeb/static/dist")
+	e.File("/docs/api/swagger.json", "C:/Users/rxri2/go/projects/GameMasterWeb/cmd/api/docs/swagger.json")
 
 	return e
 }
--- cmd/api/showUsersHandler.go
+++ cmd/api/showUsersHandler.go
@@ -6,6 +6,11 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// @Summary Получить всех пользователей
+// @Description Получить информацию о пользователе по его ID
+// @Produce json
+// @Success 200 {object} User
+// @Failure 404 {string} string
 func (app *application) showUsersHandler(c echo.Context) error {
 	return c.String(http.StatusOK, "HIIIII!")
 }
