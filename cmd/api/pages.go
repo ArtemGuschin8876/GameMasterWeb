d325202ac749647fc907901ca0543a8c8fde9c1d
ArtemGuschin8876 <newmsiartem@gmail.com>
2024-06-09 20:04:59 +0400

    added html

---
+++ cmd/api/pages.go
@@ -0,0 +1,5 @@
+package main
+
+// type pagesData struct {
+// 	Page *data.Users
+// }
--- cmd/api/showUsersHandler.go
+++ cmd/api/showUsersHandler.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"text/template"
 
 	"gamemasterweb.net/internal/data"
 	"github.com/labstack/echo/v4"
@@ -13,8 +15,9 @@ import (
 // @Success 200 {object} User
 // @Failure 404 {string} string
 func (app *application) showUsersHandler(c echo.Context) error {
-
+	fmt.Println(c)
 	id, err := app.readIDParam(c)
+	fmt.Println("id", id)
 	if err != nil {
 		return jsendError(c, "Id retrieval error")
 	}
@@ -23,12 +26,27 @@ func (app *application) showUsersHandler(c echo.Context) error {
 	if err != nil {
 		switch {
 		case errors.Is(err, data.ErrRecordNotFound):
-			jsendError(c, "the requested resource could not be found")
+			return jsendError(c, "the requested resource could not be found")
 		default:
-			jsendError(c, "the server was unable to process your request")
+			return jsendError(c, "the server was unable to process your request")
 		}
-
 	}
 
-	return jsendSuccess(c, user)
+	// data := &pagesData{Page: user}
+
+	files := []string{
+		"./static/ui/html/table.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		return jsendError(c, "error read template files")
+	}
+
+	err = ts.Execute(c.Response().Writer, user)
+	if err != nil {
+		return jsendError(c, "error execute template files")
+	}
+
+	return nil
 }
