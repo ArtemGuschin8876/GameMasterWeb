3a708bc23e41d0d4ccab9ab7dc6fecad77ec4550
ArtemGuschin8876 <newmsiartem@gmail.com>
2024-07-01 20:30:30 +0400

    fix

--- migrations/000001_users.up.sql
+++ migrations/000001_users.up.sql
@@ -1,13 +1,9 @@
 CREATE TABLE IF NOT EXISTS users (
     id bigserial PRIMARY KEY,
     name TEXT NOT NULL, 
-    nickname TEXT NOT NULL UNIQUE,
-    email TEXT NOT NULL UNIQUE, 
+    nickname TEXT NOT NULL,
+    email TEXT NOT NULL, 
     city TEXT NOT NULL, 
     about TEXT NOT NULL,
     image TEXT NOT NULL
 );
-
-CREATE UNIQUE INDEX ON users (email);
-CREATE UNIQUE INDEX ON users (nickname);
-
